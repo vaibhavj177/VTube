d67e0621d5d462d49e86559ed02c6b4eb9d9c702
Vaibhav <vaibhav10127jain@gmail.com>
2021-11-03 22:33:04 +0530

    Routers congigured and layouts header footer are set up and styled

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
 const port = 8000;
+const sassMiddleware = require('node-sass-middleware');
+
+
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}));
+
+app.use(express.static('./assets'));
+app.use(expressLayouts);
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use('/', require('./routes'));
 app.listen(port, function(err){
     if(err){
         console.log(`Error in starting the server: ${err}`);
